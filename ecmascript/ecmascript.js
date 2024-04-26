a3f87f369404d2adebf0f0384590b64a1d93720c
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-26 17:43:27 -0300

    const e let var na function

--- ecmascript/ecmascript.js
+++ ecmascript/ecmascript.js
@@ -10,4 +10,19 @@ if (estaFrio) {
 const cpf = '123.456.000-99';
 // cpf = '789.101.132-78'
 console.log(cpf);
+var segredo = 'externo!';
+function revelar() {
+    let segredo = 'interno';
+    console.log(segredo);
+}
+revelar();
+console.log(segredo);
+{
+    const def = 'def';
+    console.log(def);
+}
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+// console.log(i)
 //# sourceMappingURL=ecmascript.js.map
--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -11,3 +11,22 @@ if (estaFrio) {
 const cpf: string = '123.456.000-99'
 // cpf = '789.101.132-78'
 console.log(cpf)
+
+var segredo = 'externo!'
+function revelar() {
+    let segredo = 'interno'
+    console.log(segredo)
+}
+revelar()
+console.log(segredo)
+
+
+{
+    const def = 'def'
+    console.log(def)
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+// console.log(i)
