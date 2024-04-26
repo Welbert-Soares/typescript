28da30f01c6ce3c8d913b1ce8801e47f98473d14
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-26 15:01:35 -0300

    desafio tipos de objeto

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -75,3 +75,25 @@ let usuario = {
     idade: 27
 };
 console.log(usuario);
+// Desafio
+/*
+    Criar um objeto funcionário com:
+        - Array de strings com os nomes dos supervisores
+        - Função de bater ponto que recebe a hora (número) e retorna uma string
+            -> Ponto normal (<= 8)
+            -> Fora do horário (> 8)
+*/
+let funcionario = {
+    supervisores: ['Ana', 'Fernando'],
+    baterPonto(horario) {
+        if (horario <= 8) {
+            return 'Ponto normal';
+        }
+        else {
+            return 'Fora do horário';
+        }
+    }
+};
+console.log(funcionario.supervisores);
+console.log(funcionario.baterPonto(8));
+console.log(funcionario.baterPonto(9));
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -97,3 +97,32 @@ let usuario: { nome: string, idade: number } = {
 }
 
 console.log(usuario)
+
+// Desafio
+
+/*
+    Criar um objeto funcionário com:
+        - Array de strings com os nomes dos supervisores
+        - Função de bater ponto que recebe a hora (número) e retorna uma string
+            -> Ponto normal (<= 8)
+            -> Fora do horário (> 8)
+*/
+
+let funcionario: {
+    supervisores: string[],
+    baterPonto: (horas: number) => string
+} = {
+    supervisores: ['Ana', 'Fernando'],
+    baterPonto(horario: number): string {
+        if (horario <= 8) {
+            return 'Ponto normal'
+        } else {
+            return 'Fora do horário'
+        }
+    }
+}
+
+console.log(funcionario.supervisores)
+console.log(funcionario.baterPonto(8))
+console.log(funcionario.baterPonto(9))
+
