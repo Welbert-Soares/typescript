7d30ddc44d447dde2a52ef8c4211741963516efb
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-26 17:14:41 -0300

    strictNullChecks, noUnsedParameters

--- compilador/compilador.js
+++ compilador/compilador.js
@@ -10,4 +10,14 @@ function soma(a, b) {
 let qualquerCoisa;
 qualquerCoisa = 12;
 qualquerCoisa = 'abc';
+function saudar(isManha, horas) {
+    let saudacao;
+    if (isManha) {
+        saudacao = 'Bom dia!';
+    }
+    else {
+        saudacao = 'Tenha uma boa vida!';
+    }
+    return saudacao;
+}
 //# sourceMappingURL=compilador.js.map
--- compilador/compilador.ts
+++ compilador/compilador.ts
@@ -13,3 +13,13 @@ function soma(a: any, b: any) {
 let qualquerCoisa;
 qualquerCoisa = 12;
 qualquerCoisa = 'abc';
+
+function saudar(isManha: boolean, horas: number): string{
+    let saudacao: string;
+    if (isManha) {
+        saudacao = 'Bom dia!';
+    } else {
+        saudacao = 'Tenha uma boa vida!';
+    }
+    return saudacao;
+}
