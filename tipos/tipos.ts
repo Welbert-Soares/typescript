58456c5ef1e54c9343ae05f025f3e53d1ddd5733
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-26 12:13:26 -0300

    tipo any com javascript

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -54,3 +54,9 @@ console.log(minhaCor)
 console.log(Cor.Azul)
 console.log(Cor.Laranja, Cor.Amarelo)
 console.log(Cor.Vermelho)
+
+// any
+let carro: any = 'BMW'
+console.log(carro)
+carro = { marca: 'BMW', ano: 2019 }
+console.log(carro)
