7bf6162c023dfbeb850a1df3aa37d6eccb63a8e2
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-26 11:57:17 -0300

    inicializando tipos em variaveis

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -12,3 +12,8 @@ console.log(idade);
 let possuiHobbies = false;
 // possuiHobbies = 1
 console.log(possuiHobbies);
+// tipos explícitos
+let minhaIdade;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
+// minhaIdade = '27'
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,17 +1,23 @@
 // string
 
-let nome = 'João'
+let nome: string = 'João'
 console.log(nome)
 // nome = 28
 
 // numbers
-let idade = 27
+let idade: number = 27
 // idade = 'Ana'
 idade = 27.5
 
 console.log(idade)
 
 // boolean
-let possuiHobbies = false
+let possuiHobbies: boolean = false
 // possuiHobbies = 1
 console.log(possuiHobbies)
+
+// tipos explícitos
+let minhaIdade: number
+minhaIdade = 27
+console.log(typeof minhaIdade)
+// minhaIdade = '27'
