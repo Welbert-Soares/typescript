b7bb3905607f37c5aeabc94af51ee5ae5d151ab7
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-26 17:46:02 -0300

    arron fuction

--- ecmascript/ecmascript.js
+++ ecmascript/ecmascript.js
@@ -25,4 +25,11 @@ for (let i = 0; i < 10; i++) {
     console.log(i);
 }
 // console.log(i)
+// Arrow Function
+function somar(n1, n2) {
+    return n1 + n2;
+}
+console.log(somar(2, 2));
+const subtrair = (n1, n2) => n1 - n2;
+console.log(subtrair(2, 3));
 //# sourceMappingURL=ecmascript.js.map
--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -30,3 +30,14 @@ for (let i = 0; i < 10; i++) {
     console.log(i)
 }
 // console.log(i)
+
+// Arrow Function
+function somar(n1: number, n2: number): number {
+    return n1 + n2
+}
+console.log(somar(2, 2))
+
+const subtrair = (n1: number, n2: number): number => n1 - n2
+console.log(subtrair(2, 3))
+
+
