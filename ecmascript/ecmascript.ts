8c339883e1c8e5137f2f4164ebf07063879a6aae
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-29 09:03:56 -0300

    exercicios

--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -146,3 +146,33 @@ const boasVindas = `
 console.log(boasVindas)
 console.log(`${(1 + 1) * 30}`)
 console.log(`Motor: ${caracteristicas[0]}`)
+
+// Exercício 1
+const dobro = (valor: number): number => valor * 2
+console.log(dobro(10))
+
+// Exercício 2
+const dizerOla = (nome:string | undefined = 'Pessoa'): void => console.log(`Olá, ${nome}`)
+dizerOla()
+dizerOla('Anna')
+
+// Exercício 3
+const nums = [-3, 33, 38, 5]
+console.log(Math.min(...nums))
+
+// Exercício 4
+const array = [55, 20]
+array.push(...nums)
+console.log(array)
+
+// Exercício 5
+const notas = [8.5, 6.3, 9.4]
+const [nota1, nota2, nota3] = notas
+console.log(nota1, nota2, nota3)
+
+// Exercício 6
+const cientista = {primeiroNome: 'Will', experiencia: 12}
+const {primeiroNome, experiencia} = cientista
+console.log(primeiroNome, experiencia)
+
+// Callback
