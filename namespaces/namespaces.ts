4663c3c9f4fcdee821ae0dbaf8acdc8223fb06a2
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-29 14:18:33 -0300

    namespaces

---
+++ namespaces/namespaces.ts
@@ -0,0 +1,16 @@
+namespace Areas{
+    const PI = 3.1416;
+    
+    export function Circunferencia(raio: number): number {
+      return PI * Math.pow(raio, 2);
+    }
+    
+    export function Retangulo(base: number, altura: number): number {
+      return base * altura;
+    }  
+}
+
+const PI = 2.99;
+console.log(Areas.Circunferencia(10));
+console.log(Areas.Retangulo(10, 20));
+console.log(PI);
