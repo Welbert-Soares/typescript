849177f089170daad4e6a06918f8a8450decfeeb
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-29 13:33:25 -0300

    classes abstradas

--- classes/classes.ts
+++ classes/classes.ts
@@ -177,3 +177,34 @@ class Matematica {
 // console.log(m1.areaCirc(4));
 
 console.log(Matematica.areaCirc(4));
+
+// Classe Abstrata
+abstract class Calculo {
+  protected resultado: number = 0;
+
+  abstract executar(...numeros: number[]): void;
+
+  getResultado(): number {
+    return this.resultado;
+  }
+}
+
+class Soma extends Calculo {
+  executar(...numeros: number[]): void {
+    this.resultado = numeros.reduce((t, a) => t + a);
+  }
+}
+
+class Multiplicacao extends Calculo {
+  executar(...numeros: number[]): void {
+    this.resultado = numeros.reduce((t, a) => t * a);
+  }
+}
+
+let c1 = new Soma();
+c1.executar(2, 3, 4, 5);
+console.log(c1.getResultado());
+
+c1 = new Multiplicacao();
+c1.executar(2, 3, 4, 5);
+console.log(c1.getResultado());
