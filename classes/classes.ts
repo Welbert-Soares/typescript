04ec4d549830f771f51b5846cbd6a3d83fcf1e91
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-29 13:11:50 -0300

    membros estáticos

--- classes/classes.ts
+++ classes/classes.ts
@@ -162,3 +162,18 @@ console.log(pessoa1);
 
 pessoa1.idade = -3;
 console.log(pessoa1);
+
+// Atributos e métodos estáticos
+class Matematica {
+  static PI: number = 3.1416;
+
+  static areaCirc(raio: number): number {
+    return this.PI * raio * raio;
+  }
+}
+
+// const m1 = new Matematica();
+// m1.PI = 4.2;
+// console.log(m1.areaCirc(4));
+
+console.log(Matematica.areaCirc(4));
