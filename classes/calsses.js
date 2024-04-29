08a90496eb5a9345318f0ab6c572668c51635c22
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-29 10:38:36 -0300

    class com os puclics e constructor

--- classes/calsses.js
+++ classes/calsses.js
@@ -13,4 +13,19 @@ console.log(aniversario);
 const casamento = new Data(); // posso omitir os parâmetros
 casamento.ano = 2024;
 console.log(casamento);
+//Refatorando a classe Data
+class DataEsperta {
+    constructor(dia = 1, mes = 1, ano = 1970) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
+const aniversarioEsperto = new DataEsperta(25, 2, 1999);
+aniversarioEsperto.dia = 27;
+console.log(aniversarioEsperto.dia);
+console.log(aniversarioEsperto);
+const casamentoEsperto = new DataEsperta(); // posso omitir os parâmetros
+casamentoEsperto.ano = 2024;
+console.log(casamentoEsperto);
 //# sourceMappingURL=calsses.js.map
--- classes/calsses.ts
+++ classes/calsses.ts
@@ -1,23 +1,39 @@
 class Data {
-    // publico por padrão
-    public dia: number;
-    public mes: number;
-    ano: number;
+  // publico por padrão
+  public dia: number;
+  public mes: number;
+  ano: number;
 
-    constructor(dia: number = 1, mes: number = 1, ano: number = 1970) {
-        this.dia = dia;
-        this.mes = mes;
-        this.ano = ano;
-    }
+  constructor(dia: number = 1, mes: number = 1, ano: number = 1970) {
+    this.dia = dia;
+    this.mes = mes;
+    this.ano = ano;
+  }
 }
 
-const aniversario = new Data(25, 2, 1999)
-aniversario.dia = 27
-console.log(aniversario.dia)
-console.log(aniversario)
+const aniversario = new Data(25, 2, 1999);
+aniversario.dia = 27;
+console.log(aniversario.dia);
+console.log(aniversario);
 
-const casamento = new Data() // posso omitir os parâmetros
-casamento.ano = 2024
-console.log(casamento)
+const casamento = new Data(); // posso omitir os parâmetros
+casamento.ano = 2024;
+console.log(casamento);
 
+//Refatorando a classe Data
+class DataEsperta {
+  constructor(
+    public dia: number = 1,
+    public mes: number = 1,
+    public ano: number = 1970
+  ) {}
+}
 
+const aniversarioEsperto = new DataEsperta(25, 2, 1999);
+aniversarioEsperto.dia = 27;
+console.log(aniversarioEsperto.dia);
+console.log(aniversarioEsperto);
+
+const casamentoEsperto = new DataEsperta(); // posso omitir os parâmetros
+casamentoEsperto.ano = 2024;
+console.log(casamentoEsperto);
