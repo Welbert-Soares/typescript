d5ae0fad2ca893e9a5ca3c2b345f070777f3af07
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-30 18:11:18 -0300

    generics na classe

--- generics/generics.js
+++ generics/generics.js
@@ -12,4 +12,9 @@ function echoMelhorado(objeto) {
 console.log(echoMelhorado('João').length);
 console.log(echoMelhorado(27));
 console.log(echoMelhorado({ nome: 'João', idade: 27 }).nome);
+// Generics disponíveis na API
+const avaliacoes = [10, 9.3, 7.7];
+avaliacoes.push(8.4);
+// avaliacoes.push('5.5')
+console.log(avaliacoes);
 //# sourceMappingURL=generics.js.map
